1ae7d89ab51766592e18977bc35183c3a87d85e5
nfoissac <nicola.foissac@gmail.com>
2018-05-18 14:18:29 +0200

    fix instrument

--- helpers/sparqlHelper.py
+++ helpers/sparqlHelper.py
@@ -26,14 +26,14 @@ class SPARQLHelper:
          SELECT ?node ?title ?author ?path ?genre
          WHERE {
          ?node strm:subject [?li ?subject].
-         ?node strm:comment [?li ?description].
+         ?node strm:comment [?li ?comment].
          ?node strm:title ?title.
          ?node strm:author ?author.
          ?node strm:composer ?composer.
          ?node strm:genre ?genre.
          ?node strm:path ?path  
          '''
-        self.__query = self.__query + '?node strm:instrument [?li ?instrument ].' if len(instrument) > 0 else self.__query + 'OPTIONAL { ?node strm:instrument [?li ?instrument ] }. '
+        self.__query = self.__query + '. ?node strm:instrument [?li ?instrument ] .' if len(instrument) > 0 else self.__query + 'OPTIONAL { ?node strm:instrument [?li ?instrument ] }. '
         self.__query = self.__query + '?node strm:rating ?rating' if len(rating) > 0 else self.__query + 'OPTIONAL { ?node strm:rating ?rating } '
         if len(instrument) > 0 or len(subject) > 0 or len(description) or len(title) > 0 or len(author) > 0 or len(composer) > 0 or len(genre) > 0 or len(rating) :
             self.__query = self.__query + '. filter('
@@ -59,7 +59,7 @@ class SPARQLHelper:
 
     def __seek_description(self,description=[]):
         for item in description :
-            self.__query = self.__query + """?description = '"""+ item + """' || """
+            self.__query = self.__query + """?comment = '"""+ item + """' || """
 
     def __seek_title(self,title=[]):
         for item in title :
@@ -80,4 +80,3 @@ class SPARQLHelper:
     def __seek_rating(self,rating=[]):
         for item in rating :
             self.__query = self.__query + """?rating = '"""+ item + """' || """
-
