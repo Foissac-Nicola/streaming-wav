52edb98fe2ccc59ac94963c804c91f352a3749ab
Loic Favreliere <loic.favreliere@etudiant.univ-lr.fr>
2018-05-18 17:19:23 +0200

    Send path to play music

--- streamapp/views.py
+++ streamapp/views.py
@@ -1,7 +1,25 @@
+import socket, sys
 from flask import (render_template, request, jsonify)
 from streamapp import app
 
 from helpers.sparqlHelper import SPARQLHelper
+
+
+@app.route('/play', methods=['POST'])
+def play():
+    path = request.form['path']
+    msg = "POST /play HTTP/1.1 200 \n\n" + path
+
+    m_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        m_socket.connect(("127.0.0.1", 7800))
+        m_socket.send(msg.encode("Utf8"))
+        m_socket.close()
+    except socket.error as error:
+        print("Play music failed - Error:", error)
+
+    return jsonify(status=200)
 
 
 @app.route('/search', methods=['POST'])
