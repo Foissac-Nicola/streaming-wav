500658fc53cfd698db27ec4af6bae0bd408a6d37
nfoissac <nicola.foissac@gmail.com>
2018-05-22 12:54:45 +0200

    comment sparql

--- helpers/sparqlHelper.py
+++ helpers/sparqlHelper.py
@@ -4,12 +4,20 @@
 import rdflib as rdf
 
 class SPARQLHelper:
+    """
+        This class provide some method for querying bdd
+    """
 
     def __init__(self,path) -> None:
         self.__graph = rdf.Graph()
         self.__graph.parse(path)
 
     def exec_query(self,**kwargs):
+        """
+            This method exec query
+        :param kwargs: instrument=[], subject=[],description=[], title=[], author=[], composer=[], genre=[], rating=[], rigth=[]
+        :return: list of dict result
+        """
         self.__build_query(**kwargs)
 
         print(self.__query)
@@ -22,6 +30,19 @@ class SPARQLHelper:
         return ret
 
     def __build_query(self,instrument=[], subject=[],description=[], title=[], author=[], composer=[], genre=[], rating=[], rigth=[]):
+        """
+            This build query in function of argument.
+        :param instrument: list of instrument
+        :param subject: list of subject
+        :param description: list of description
+        :param title: list of title
+        :param author: list of author
+        :param composer: list of composer
+        :param genre: list of genre
+        :param rating: list of rating
+        :param rigth: list of right
+        :return:
+        """
         self.__query = '''PREFIX strm: <urn:/streaming/>
          SELECT ?node ?title ?author ?path ?genre
          WHERE {
@@ -50,33 +71,73 @@ class SPARQLHelper:
         self.__query = self.__query + '} group by ?node ORDER BY ?title'
 
     def __seek_instrument(self,instrument=[]):
+        """
+            Add to query filter by instrument
+        :param instrument: list of instrument
+        :return: None
+        """
         for item in instrument :
             self.__query = self.__query + """?instrument = '"""+ item + """' || """
 
     def __seek_subject(self,subject=[]):
+        """
+        Add to query filter by subject
+        :param subject: list of subject
+        :return: None
+        """
         for item in subject :
             self.__query = self.__query + """?subject = '"""+ item + """' || """
 
     def __seek_description(self,description=[]):
+        """
+        Add to query filter by description
+        :param description: list of description
+        :return: None
+        """
         for item in description :
             self.__query = self.__query + """?comment = '"""+ item + """' || """
 
     def __seek_title(self,title=[]):
+        """
+        Add to query filter by title
+        :param title: list of title
+        :return: None
+        """
         for item in title :
             self.__query = self.__query + """?title = '"""+ item + """' || """
 
     def __seek_author(self,author=[]):
+        """
+        Add to query filter by author
+        :param author: list of author
+        :return: None
+        """
         for item in author :
             self.__query = self.__query + """?author = '"""+ item + """' || """
 
     def __seek_composer(self,composer=[]):
+        """
+        Add to query filter by composer
+        :param composer: list of composer
+        :return: None
+        """
         for item in composer :
             self.__query = self.__query + """?composer = '"""+ item + """' || """
 
     def __seek_genre(self,genre=[]):
+        """
+        Add to query filter by genre
+        :param genre: list of genre
+        :return: None
+        """
         for item in genre :
             self.__query = self.__query + """?genre = '"""+ item + """' || """
 
     def __seek_rating(self,rating=[]):
+        """
+        Add to query filter by rating
+        :param rating: list of rating
+        :return: None
+        """
         for item in rating :
             self.__query = self.__query + """?rating = '"""+ item + """' || """
